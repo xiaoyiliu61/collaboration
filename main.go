2a6d7f84022d97ee1f2752a8b32e68f8de99510d
xiaoyiliu61 <2693596075@qq.com>
2020-12-24 19:06:20 +0800

    test

--- main.go
+++ main.go
@@ -9,15 +9,12 @@ import (
 func main() {
 
 	fmt.Println("hello world")
-<<<<<<< HEAD
+
 	fmt.Println("hello liuYi")
-	beego.Run()
-=======
 
 	fmt.Println("hi my name is zihanhu")
 
-	fmt.Println("hello liuYi")
->>>>>>> remotes/origin/zihan
+	beego.Run()
 
 }
 
