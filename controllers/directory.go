354311fc355671cc27ea99cc975cbaaf6a062eb5
jiujideshangxian <1529124973@qq.com>
2021-01-03 19:06:04 +0800

    test5

--- controllers/directory.go
+++ controllers/directory.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"collaboration/utils"
 	"github.com/astaxie/beego"
 )
 
@@ -11,16 +12,37 @@ type DirectoryController struct {
 
 
 func (D *DirectoryController)Post() {
-	/*var user1 models.User1
-	a:=user1.Num2
-	b:=user1.Num
-    fmt.Println(a)
-    fmt.Println(b)
 
-	if a==b {*/
-		D.TplName="login.html"
-	/*}
-	D.TplName="bitDirectory.html"*/
+/*	fmt.Println("当前区块的数量是",utils.GetBlockCount())
+	fmt.Println("最新区块的hash是",utils.GetBestBlockHash())
+	fmt.Println("根据高度0获取高度为0的hash值",utils.GetBlockHashByHeight(0))
+	fmt.Println("获取新的地址",utils.GetNewAddress("123456",utils.LEGACY))*/
+
+
+		D.TplName="bitDirectory.html"
+	D.Data["d"] =utils.GetDifficulty()
+	D.Data["b"] =utils.GetBestBlockHash()
+	D.Data["h"] =utils.GetBlockHashByHeight(0)
+	D.Data["c"] =utils.GetBlockCount()
+
+
+
+}
+
+func (D *DirectoryController)Get() {
+
+	/*	fmt.Println("当前区块的数量是",utils.GetBlockCount())
+		fmt.Println("最新区块的hash是",utils.GetBestBlockHash())
+		fmt.Println("根据高度0获取高度为0的hash值",utils.GetBlockHashByHeight(0))
+		fmt.Println("获取新的地址",utils.GetNewAddress("123456",utils.LEGACY))*/
+
+	D.Data["d"] =utils.GetDifficulty()
+	D.Data["b"] =utils.GetBestBlockHash()
+	D.Data["h"] =utils.GetBlockHashByHeight(0)
+	D.Data["c"] =utils.GetBlockCount()
+
+	//D.TplName="login.html"
+
 
 }
 
--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"collaboration/db_mysql"
 	_ "collaboration/routers"
 	"collaboration/utils"
 	"fmt"
@@ -9,12 +8,24 @@ import (
 )
 
 func main() {
-	Getdifficulty1:=utils.GetDifficulty();
-	fmt.Println("当前区块的难度值是",Getdifficulty1)
+	//区块难度值
+	//Getdifficulty1:=utils.GetDifficulty();
+
+	/*fmt.Println("当前区块的难度值是",utils.GetDifficulty())
+	fmt.Println("当前区块的数量是",utils.GetBlockCount())
+	fmt.Println("最新区块的hash是",utils.GetBestBlockHash())*/
+
+	//fmt.Println("获取新的地址",utils.GetNewAddress("123456",utils.LEGACY))
+	fmt.Println("根据高度0获取高度为0的hash值",utils.GetBlockHashByHeight(0))
+
 	//连接数据库
-	db_mysql.Connect()
 
-
+	   /* db_mysql.Db.Exec("insert into " +
+		"block1(" +
+		"id,height,hash1)" +
+		"values(" +
+		"?,?,?)",
+		1,0,utils.GetBlockHashByHeight(0))*/
 
 
 	//设置静态资源文件映射
@@ -22,6 +33,7 @@ func main() {
 	beego.SetStaticPath("/directory.html", "./views/bitDirectory.html")
 	beego.SetStaticPath("/bitDirectory.html", "./views/bitDirectory.html")
 	beego.SetStaticPath("/login.html", "./views/login.html")
+	beego.SetStaticPath("/index", "./views/index.html")
 
 	beego.SetStaticPath("/js", "./static/js")
 	beego.SetStaticPath("/css", "./static/css")
